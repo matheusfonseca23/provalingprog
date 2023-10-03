21fc9e19aea6d0cbf2252ebf99e8c9e37d4a0f33
preti-joao <108187064+preti-joao@users.noreply.github.com>
2023-10-03 16:47:02 -0400

    feat: prova1.c

---
+++ prova1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+PROVA PRÁTICA DE LINGUAGEM DE PROGRAMAÇÃO
+Ao término enviar e-mail conforme modelo:
+Para:       preti.joao@ifmt.edu.br
+Assunto:    Prova 1 de Linguagem de Programação 2023/2
+Mensagem:   NOME COMPLETO DO ESTUDANTE
+Anexo:      prova1.py
+*/
+
+//1. Faça um programa que leia o valor unitário de um produto,
+//   a quantidade desejada e imprima o valor total a pagar. (2,5pt)
+void q01() {
+
+}
+
+//2. Faça um programa que leia 3 números e imprima o maior deles. (2,5pt)
+void q02() {
+
+}
+
+//3. Faça um programa que permita entrar com o nome, a nota da prova 1 e a nota
+//   da prova 2 de um aluno. O programa deve imprimir o nome, a nota da prova 1,
+//   a nota da prova 2, a média das notas e uma das mensagens: "Aprovado",
+//   "Reprovado" ou "em Prova Final"(a média é 7 para aprovação, menor que 3 para
+//   reprovação e as demais em prova final). (2,5pt)
+void q03() {
+
+}
+
+//4. Faça um programa que apresente um menu com 4 opções:
+//   [1] - Cadastrar
+//   [2] - Excluir
+//   [3] - Pesquisar
+//   [0] - Sair
+//   O usuário deve escolher uma opção e o programa deve imprimir uma mensagem 
+//   ao entrar em cada opção do menu. O programa só deve encerrar quando a opção
+//   escolhida for zero (0). (2,5pt)
+void q04() {
+
+}
+
+int main() {
+  q01();
+  q02();
+  q03();
+  q04();
+  
+  return EXIT_SUCCESS;
+}
